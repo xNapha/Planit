6ff6732a49816d93f9e651426911b595d6cb4402
NathanJCai <nathancai1997@gmail.com>
2022-07-14 16:15:38 +1000

    Update FibSequence.js

--- Task Two/FibSequence.js
+++ Task Two/FibSequence.js
@@ -1,28 +1,12 @@
 function fibSeq(n){
-    let n1 = 0, n2 = 1, fib;
-    if (n == 0){
-        return n1;
+    if (n < 2){
+        return n;
     }
-    for(let i = 2; i <= n; i++){
-        fib = n1 + n2;
-        n1 = n2;
-        n2 = fib;
-    } return fib;
+    return fibSeq(n-1) + fibSeq(n-2);
+    ;
 }
 
-function findIndexN(F){
-    let n1=0, n2=1, fib = 0, n = 0;
-    while(fib < F){
-        fib = n1 + n2;
-        n1 = n2;
-        n2 = fib;
-        n++;
-    } return n;
-        
-}
 
-console.log(fibSeq(10));
-console.log(fibSeq(17))
-console.log(fibSeq(18))
-console.log(findIndexN(2000))
+console.log(fibSeq(2));
+
 
