d0bcd782e0f6d5f9603381c5d11eb9411d8daee8
NathanJCai <nathancai1997@gmail.com>
2022-07-15 16:49:46 +1000

    Updated folder task two

--- Task Two/FibSequence.js
+++ Task Two/FibSequence.js
@@ -1,12 +1,43 @@
+let gr = (1+Math.sqrt(5))/2
+let arr = [];
 function fibSeq(n){
-    if (n < 2){
+    if(n <2){
         return n;
     }
     return fibSeq(n-1) + fibSeq(n-2);
-    ;
-}
+};
+
+function revFib(n){
+    arr.unshift(n)
+    if (n <= 1){
+        arr.unshift(n)
+        return 0;
+    }
+    n = Math.round(n/gr)
+    revFib(n)
+    return arr
+};
+
+function findFib(n){
+    if (n == 0 || n == 1){
+        return n + " is a Fibonacci number"
+    } else if(n < 0){
+        return "Please enter a positive integer"
+    }
+    let x = revFib(n).length;
+    let y = fibSeq(x)
+    if(revFib(n)[x-1] == y){
+        return n + " is a Fibonacci number"
+    } else if (fibSeq(x+1) - n < n - fibSeq(x)){
+        return (x + 1) + " is the closest index in the fibonacci sequence"
+    }
+    return x + " is the closest index in the fibonacci sequence"
+};
 
 
-console.log(fibSeq(2));
+console.log(findFib(10))
+console.log(fibSeq(6))
+console.log(fibSeq(7))
+
 
 
