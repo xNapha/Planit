b3015077e98e3e8cd311d7606d9600195ff3f749
NathanJCai <nathancai1997@gmail.com>
2022-07-15 17:08:58 +1000

    Update FibSequence.js

--- Task Two/FibSequence.js
+++ Task Two/FibSequence.js
@@ -8,34 +8,34 @@ function fibSeq(n){
 };
 
 function revFib(n){
-    arr.unshift(n)
+    arr.unshift(n);
     if (n <= 1){
         arr.unshift(n)
-        return 0;
-    }
-    n = Math.round(n/gr)
-    revFib(n)
+        return;
+    };
+    n = Math.round(n/gr);
+    revFib(n);
     return arr
 };
 
 function findFib(n){
     if (n == 0 || n == 1){
-        return n + " is a Fibonacci number"
+        return n + " is a Fibonacci number";
     } else if(n < 0){
-        return "Please enter a positive integer"
-    }
+        return "Please enter a positive integer";
+    };
     let x = revFib(n).length;
-    let y = fibSeq(x)
+    let y = fibSeq(x);
     if(revFib(n)[x-1] == y){
-        return n + " is a Fibonacci number"
+        return n + " is a Fibonacci number";
     } else if (fibSeq(x+1) - n < n - fibSeq(x)){
-        return (x + 1) + " is the closest index in the fibonacci sequence"
+        return (x + 1) + " is the closest index in the fibonacci sequence";
     }
-    return x + " is the closest index in the fibonacci sequence"
+    return x + " is the closest index in the fibonacci sequence";
 };
 
 
-console.log(findFib(10))
+console.log(findFib(11))
 console.log(fibSeq(6))
 console.log(fibSeq(7))
 
